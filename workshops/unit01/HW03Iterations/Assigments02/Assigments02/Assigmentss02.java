29f7705bd0c7cea1d460e50a613e62e356ff7d1e
alejo201 <kevin2001bizarria@gmail.com>
2021-01-14 16:40:07 -0500

    HW03Iterations-Assigments02

---
+++ workshops/unit01/HW03Iterations/Assigments02/Assigments02/Assigmentss02.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assigmentss02;
+
+/**
+ *
+ * @author kevin Toapanta
+ */
+public class Assigmentss02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int i;
+    i = 0;
+    System.out.println("=== This is a while loop from 1 to 43 ===");
+    
+    while( i < 43){
+        System.out.println("i ->" +( i + 1));
+        // i +=1;
+        i++;
+        //i =1}
+        {  
+    
+    System.out.println("first 10 numbers multiples of 5");
+    
+    int stop;
+    stop=51;
+    for(int j=5 ; j < stop ; j +=5 ){
+    System.out.println("odd number ->" + j);
+   
+    }
+    
+    
+    System.out.println("Multiplication Tables : 43");
+    int top=43;
+    int product=0;
+    int table=43;
+    for (int j = 1 ; j <= top ; j++){
+    product =table * j;
+    System.out.println("43 * " + j + " = " + product);
+     
+    
+    
+    }
+        }
+    }         
+    }
+}
+
+    
+
