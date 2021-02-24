a3d7a5dc578a1e0dc5155ffff1eb837c31428a01
alejo201 <kevin2001bizarria@gmail.com>
2021-02-23 23:25:43 -0500

    HW15-MatrixOperation

---
+++ assignments/unit 2/HW15-MatrixOperation/HW15-MatrixOperation/src/hw15/matrixoperation/HW15MatrixOperation.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw15.matrixoperation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP-LAPTOP-14
+ */
+public class HW15MatrixOperation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+     System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+     System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+     System.out.println("           Kevin Toapanta                  ");
+     System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+     System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+     System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+        
+        Scanner input = new Scanner(System.in);
+        int n;
+        int m;
+        int A[][];
+        int B[][];
+        int C[][];
+        
+        System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+       
+        System.out.println("enter matrix dimension ");
+        System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+         System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+        System.out.println("Row -->");
+        System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+        n = input.nextInt();
+        System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+        System.out.println("Column -->");
+        System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+        m = input.nextInt();
+        System.out.println(" ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ");
+        System.out.println("enter values : " + n + "  row   " + m + " column ");
+        System.out.println(" ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ");
+
+        A = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                A[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("enter values ​​for matrix B : " + n + " in row and  " + m + " in column");
+        B = new int[n][m];
+        
+        
+        
+        
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                B[i][j] = input.nextInt();
+            }
+        }
+        
+        
+
+        C = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        
+        
+        System.out.println(" the result of the matrix A + B = ");
+        for (int i = 0; i < n; i++) {
+            System.out.println("");
+            for (int j = 0; j < m; j++) {
+                System.out.print("\t " + C[i][j]);
+
+            }
+        }
+        System.out.println("");
+    }
+
+}
+  
